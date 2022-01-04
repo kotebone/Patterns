b5bb0c8eb5c49cfcc29e866309ad7ccdf98fc93e
e.fedoseeva <tetstest@list.ru>
2022-01-05 02:40:00 +0300

    4.1. Репортинг

--- src/test/java/ru/netology/test/AppOrder.java
+++ src/test/java/ru/netology/test/AppOrder.java
@@ -1,14 +1,12 @@
 package ru.netology.test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
@@ -18,6 +16,16 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 class AppOrderTests {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
